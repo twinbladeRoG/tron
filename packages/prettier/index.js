51e67f918b6e2ef79fd886f1b1005a596c356ddd
Sohan Dutta <sohan.dutta11@gmail.com>
2025-11-23 21:41:00 +0530

    :sparkles: feat(root): setup linter and precommit hook

---
+++ packages/prettier/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "preserve",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  endOfLine: "lf",
+  trailingComma: "es5",
+  bracketSameLine: true,
+  arrowParens: "always",
+};
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+import baseConfig from '@tron/prettier';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  ...baseConfig,
+  plugins: ['prettier-plugin-packagejson']
+};
+
+export default config;
