305ac4fd7fad9dd5a3195b289984a3dd2234ac5f
Sohan Dutta <sohan.dutta11@gmail.com>
2025-12-27 15:19:54 +0530

    :lipstick: style(web): change usage stats position

--- apps/web/src/components/modules/usage-logs/UsageLogs.tsx
+++ apps/web/src/components/modules/usage-logs/UsageLogs.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Icon } from '@iconify/react';
-import { Button, Skeleton, Table } from '@mantine/core';
+import { Button, Card, Skeleton, Table } from '@mantine/core';
 import { DatePickerInput, type DatesRangeValue } from '@mantine/dates';
 import { useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
@@ -59,7 +59,9 @@ const UsageLogs: React.FC<UsageLogsProps> = ({ className }) => {
 
   return (
     <div className={cn(className)}>
-      <form className="mb-7 flex gap-4 rounded-2xl p-4 dark:bg-slate-950" onSubmit={handleSubmit}>
+      <form
+        className="mb-7 flex flex-col gap-4 rounded-2xl p-4 md:flex-row dark:bg-slate-950"
+        onSubmit={handleSubmit}>
         <Controller
           control={form.control}
           name="model"
@@ -120,74 +122,72 @@ const UsageLogs: React.FC<UsageLogsProps> = ({ className }) => {
         </Button>
       </form>
 
-      <Table stickyHeader stickyHeaderOffset={60}>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th className="whitespace-nowrap">Total Tokens</Table.Th>
-            <Table.Th className="whitespace-nowrap">Prompt Tokens</Table.Th>
-            <Table.Th className="whitespace-nowrap">Completion Tokens</Table.Th>
-            <Table.Th className="whitespace-nowrap">Reasoning Tokens</Table.Th>
-            <Table.Th className="whitespace-nowrap">Time</Table.Th>
-            <Table.Th className="whitespace-nowrap">Total Cost</Table.Th>
-            <Table.Th className="whitespace-nowrap">Used On</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
+      <div className="mb-7 grid grid-cols-1 gap-4 sm:grid-cols-3">
+        <Card>
+          Total Tokens:{' '}
+          <strong>
+            {numberFormatter.format(
+              (logs.data ?? []).reduce((acc, val) => acc + val.total_tokens, 0)
+            )}
+          </strong>
+        </Card>
+        <Card>
+          Total Time:{' '}
+          <strong>
+            {formatDuration((logs.data ?? []).reduce((acc, val) => acc + val.time, 0))}
+          </strong>
+        </Card>
+        <Card>
+          Total Cost:{' '}
+          <strong>
+            {currencyFormatter.format(
+              (logs.data ?? []).reduce((acc, val) => acc + val.total_cost, 0)
+            )}
+          </strong>
+        </Card>
+      </div>
 
-        <Table.Tbody>
-          {logs.isFetching ? (
+      <Table.ScrollContainer minWidth={500}>
+        <Table stickyHeader stickyHeaderOffset={0}>
+          <Table.Thead>
             <Table.Tr>
-              <Table.Td colSpan={6}>
-                <Skeleton h={40} mb="xs" />
-                <Skeleton h={40} mb="xs" />
-                <Skeleton h={40} mb="xs" />
-              </Table.Td>
+              <Table.Th className="whitespace-nowrap">Total Tokens</Table.Th>
+              <Table.Th className="whitespace-nowrap">Prompt Tokens</Table.Th>
+              <Table.Th className="whitespace-nowrap">Completion Tokens</Table.Th>
+              <Table.Th className="whitespace-nowrap">Reasoning Tokens</Table.Th>
+              <Table.Th className="whitespace-nowrap">Time</Table.Th>
+              <Table.Th className="whitespace-nowrap">Total Cost</Table.Th>
+              <Table.Th className="whitespace-nowrap">Used On</Table.Th>
             </Table.Tr>
-          ) : (
-            logs.data?.map((log) => (
-              <Table.Tr key={log.id}>
-                <Table.Td>{log.total_tokens}</Table.Td>
-                <Table.Td>{log.prompt_tokens}</Table.Td>
-                <Table.Td>{log.completion_tokens}</Table.Td>
-                <Table.Td>{log.reasoning_tokens}</Table.Td>
-                <Table.Td>{formatDuration(log.time)}</Table.Td>
-                <Table.Td>{currencyFormatter.format(log.total_cost)}</Table.Td>
-                <Table.Td className="whitespace-nowrap">
-                  {dayjs(log.end_time).format('h:mm a, DD MMM YYYY')}
+          </Table.Thead>
+
+          <Table.Tbody>
+            {logs.isFetching ? (
+              <Table.Tr>
+                <Table.Td colSpan={7}>
+                  <Skeleton h={40} mb="xs" />
+                  <Skeleton h={40} mb="xs" />
+                  <Skeleton h={40} mb="xs" />
                 </Table.Td>
               </Table.Tr>
-            ))
-          )}
-        </Table.Tbody>
-
-        <Table.Tfoot>
-          <Table.Tr>
-            <Table.Tr>
-              <Table.Td>
-                Total Tokens:{' '}
-                <strong>
-                  {numberFormatter.format(
-                    (logs.data ?? []).reduce((acc, val) => acc + val.total_tokens, 0)
-                  )}
-                </strong>
-              </Table.Td>
-              <Table.Td>
-                Total Time:{' '}
-                <strong>
-                  {formatDuration((logs.data ?? []).reduce((acc, val) => acc + val.time, 0))}
-                </strong>
-              </Table.Td>
-              <Table.Td>
-                Total Cost:{' '}
-                <strong>
-                  {currencyFormatter.format(
-                    (logs.data ?? []).reduce((acc, val) => acc + val.total_cost, 0)
-                  )}
-                </strong>
-              </Table.Td>
-            </Table.Tr>
-          </Table.Tr>
-        </Table.Tfoot>
-      </Table>
+            ) : (
+              logs.data?.map((log) => (
+                <Table.Tr key={log.id}>
+                  <Table.Td>{log.total_tokens}</Table.Td>
+                  <Table.Td>{log.prompt_tokens}</Table.Td>
+                  <Table.Td>{log.completion_tokens}</Table.Td>
+                  <Table.Td>{log.reasoning_tokens}</Table.Td>
+                  <Table.Td>{formatDuration(log.time)}</Table.Td>
+                  <Table.Td>{currencyFormatter.format(log.total_cost)}</Table.Td>
+                  <Table.Td className="whitespace-nowrap">
+                    {dayjs(log.end_time).format('h:mm a, DD MMM YYYY')}
+                  </Table.Td>
+                </Table.Tr>
+              ))
+            )}
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
     </div>
   );
 };
