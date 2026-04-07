c79c74783e8639f6238000d24479ec59cc676161
Sohan Dutta <sohan.dutta11@gmail.com>
2026-04-07 08:11:59 +0530

    :sparkles: feat(api): create separate docker build for dev and prod

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -12,7 +12,7 @@ tmux split-window -v -t "$SESSION":1.2
 tmux split-window -v -t "$SESSION":1.4
 tmux split-window -v -t "$SESSION":1.5
 
-tmux send-keys -t "$SESSION":1.1 "pnpm -F api docker:up && source ./apps/api/.venv/bin/activate && pnpm -F api dev" C-m
+tmux send-keys -t "$SESSION":1.1 "pnpm -F api docker:deps:up && source ./apps/api/.venv/bin/activate && pnpm -F api dev" C-m
 tmux send-keys -t "$SESSION":1.2 "source ./apps/api/.venv/bin/activate && pnpm -F api celery:flower" C-m
 tmux send-keys -t "$SESSION":1.3 "pnpm -F web dev" C-m
 tmux send-keys -t "$SESSION":1.4 "nvtop" C-m
